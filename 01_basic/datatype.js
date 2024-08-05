9dcd5d8e2fb1f58eb38b69eb2113ad01792a7e03
Sheetal-chouhan <120328552+Sheetal-chouhan@users.noreply.github.com>
2024-08-05 19:28:28 +0000

    Variable  && datatype

---
+++ 01_basic/datatype.js
@@ -0,0 +1,21 @@
+" use strick" // terat all jd code as new version
+
+// alert(" hello") // we are using node js, not browser
+
+console.log(3+3)
+console.log(" Sheetal")
+
+//number => 2 to power 53
+// bigint                        { for big number or large number}
+//string => " "
+// boolean => true / false
+//null => standalone value
+// undefine =>                    { like let state ;  ( variable is not define)}
+// symbole => unique
+
+// object
+
+console.log(typeof " Sheetal")  // string
+console.log(typeof null )      // object
+console.log(typeof undefined ) // undefined
+
---
+++ 01_basic/variable1.js
@@ -0,0 +1,16 @@
+const accId = 124567849
+let accEmail = " sheetal123@gmail.com"
+var accPwd = "324"
+accCity = " Bhopal"
+let accState;
+
+// accId  = 2;
+
+accEmail = " rose543@gmail.com "
+accPwd  = " 654 "
+accCity = " Newyork "
+
+
+console.log(accId);
+console.table([ accId, accEmail, accPwd, accCity, accState])
+
